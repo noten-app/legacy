7d278d85db2e55b5bc0abae8dece813b206bbe4f
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-10-19 21:48:55 +0200

    Add screen width auto-adjust

---
+++ app/script.js
@@ -0,0 +1,9 @@
+function checkResize() {
+    if (window.screen.width <= 500) {
+        document.getElementById("top_leiste_icon-name").innerHTML = '<img src="/src/img/logo.png" alt="NotenApp Logo" style="margin: 0 !important;"/>';
+    } else {
+        document.getElementById("top_leiste_icon-name").innerHTML = '<img src="/src/img/logo.png" alt="NotenApp Logo"/>NotenApp';
+    }
+}
+window.addEventListener('orientationchange', (event) => { checkResize() });
+checkResize();
