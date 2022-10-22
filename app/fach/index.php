3a43b6bfc4e26615d46c2394919f582e93c91a40
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-10-22 21:56:38 +0200

    Fix links

--- app/fach/index.php
+++ app/fach/index.php
@@ -3,7 +3,7 @@
 	session_start();
 	// If the user is logged in redirect to the app page
     if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
-        header('Location: ./account/');
+        header('Location: ../account/');
         exit;
     }
 
