7cd6c8f13b3a6acc7d4454c4134eca601f0b70bf
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-01-11 23:17:04 +0100

    [Grades] Add action corner

---
+++ grades/index.php
@@ -0,0 +1,117 @@
+<?php 
+
+    // Check login state
+    session_start();
+    require("../res/php/checkLogin.php");
+    if(!checkLogin()) header("Location: ../account/login");
+
+    // Get config
+    require("../config.php");
+
+    // DB Connection
+    $con = mysqli_connect(
+        config_db_host,
+        config_db_user,
+        config_db_password,
+        config_db_name
+    );
+    if(mysqli_connect_errno()) exit("Error with the Database");
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NotenApp</title>
+    <link rel="icon" type="image/x-icon" href="/res/img/favicon.ico" />
+    <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
+    <link rel="stylesheet" href="/res/css/fonts.css">
+    <link rel="stylesheet" href="/res/css/main.css">
+    <link rel="stylesheet" href="/res/css/navbar.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="nav_top_buttons">
+            <a href="#" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fas fa-home"></i>
+                </div>
+            </a>
+            <a href="#" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fas fa-calendar-alt"></i>
+                </div>
+            </a>
+            <a href="#" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fas fa-calendar-check"></i>
+                </div>
+            </a>
+            <a href="/grades/" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                </div>
+            </a>
+            <a href="#" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fas fa-book"></i>
+                </div>
+            </a>
+            <a href="#" class="nav-link">
+                <div class="navbar_icon">
+                    <i class="fas fa-book-open"></i>
+                </div>
+            </a>
+        </div>
+        <div class="nav_bottom_buttons">
+            <a href="#" class="nav-link nav-bottom" id="theme-link" onclick="">
+                <div class="navbar_icon" id="settings-icon">
+                    <div>
+                        <i class="fas fa-cog"></i>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="nav-link nav-bottom" id="theme-link" onclick="cycleTheme();">
+                <div class="navbar_icon" id="theme-icon">
+                    <div>
+                        <i class="fas fa-adjust"></i>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </nav>
+    <main id="main">
+        <div class="grade_list"></div>
+        <div class="grade_table"></div>
+        <div class="grade_stats">
+            <div class="grade_stats_cake">
+                <div class="cake_coming_soon">
+                    Statistics<br>Coming Soon!
+                </div>
+            </div>
+            <div class="grade_stats_average">
+                &Oslash; 4,22
+            </div>
+            <div class="grade_plus">
+                <div class="grade_plus_button grade_stats_button">
+                    <i class="fa-solid fa-square-plus"></i>
+                </div>
+            </div>
+            <div class="grade_more">
+                <div class="grade_more_button grade_stats_button">
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="/res/js/themes/themes.js"></script>
+    <script src="/res/js/shortcuts/shortcuts.js"></script>
+</body>
+
+</html>
