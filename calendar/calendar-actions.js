9a5020852154a50aff085393859aec63b008fd9e
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-02-22 18:51:52 +0100

    Move cal config to own config var

--- calendar/calendar-actions.js
+++ calendar/calendar-actions.js
@@ -1,39 +1,57 @@
+// Config
+const cal_config = {
+    headerToolbar: {
+        left: 'prev,next',
+        right: 'title'
+    },
+    navLinks: false,
+    editable: true,
+    dayMaxEvents: true,
+    locale: "en",
+    events: cal_events,
+    nowIndicator: true
+};
+
 // Load calendars
 
+grid_calendar = undefined;
+list_calendar = undefined;
+
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar_list');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        headerToolbar: {
-            left: 'prev,next',
-            right: 'title'
-        },
+    list_calendar = new FullCalendar.Calendar(document.getElementById('calendar_list'), {
         initialView: 'listMonth',
-        navLinks: false,
-        editable: true,
-        dayMaxEvents: true,
-        locale: "en",
-        events: cal_events
-    });
-    calendar.render();
-});
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar_grid');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        headerToolbar: {
-            left: 'prev,next',
-            right: 'title'
-        },
-        navLinks: false,
-        editable: true,
-        dayMaxEvents: true,
-        showNonCurrentDates: true,
-        hiddenDays: [0, 6],
-        locale: "en",
-        events: cal_events,
-        nowIndicator: true,
         eventClick: function(info) {
-            alert('Event: ' + JSON.stringify(info.event.id));
+            calClick(list_calendar, info.event);
+        },
+        dateClick: function(info) {
+            dateClick(list_calendar, info.dateStr);
         }
     });
-    calendar.render();
+    grid_calendar = new FullCalendar.Calendar(document.getElementById('calendar_grid'), {
+        showNonCurrentDates: true,
+        hiddenDays: [0, 6],
+        eventClick: function(info) {
+            calClick(grid_calendar, info.event);
+        },
+        dateClick: function(info) {
+            dateClick(grid_calendar, info.dateStr);
+        }
+    });
+    for (var key in cal_config) {
+        list_calendar.setOption(key, cal_config[key]);
+        grid_calendar.setOption(key, cal_config[key]);
+    }
+    list_calendar.render();
+    grid_calendar.render();
 });
+
+// Functions
+function calClick(calendar, event) {
+    alert('Event: ' + event.id);
+    alert('Cal: ' + calendar);
+}
+
+function dateClick(calendar, date) {
+    alert('Date: ' + date);
+    alert('Cal: ' + calendar);
+}
